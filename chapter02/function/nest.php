b72cd7b3b609e7c9e2707468f9636294a02ed939
HaradaKasumi <harada@key-p.jp>
2021-02-28 14:35:34 +0900

    20210226

--- chapter02/function/nest.php
+++ chapter02/function/nest.php
@@ -7,25 +7,37 @@
 <body>
     <?php
         /* 正の整数であれば真、そうでなければ偽を返す関数 */
-        function checkNumber(int $number)
+        function checkNumber(int $number):bool
         {
             if (!is_numeric($number) && $number > 0){
                 return FALSE;
             }
-            return $number;
+            return TRUE;
         }
 
-        $result = checkNumber(4);
-        echo $result;
 
         /* 2つの数値を足し算して返す関数 */
-
+        function add(int $a, int $b) // false または int
+        {
+            if (FALSE === checkNumber($a) || FALSE === checkNumber($b)){
+                return FALSE;
+            }
+            $result = $a + $b;
+            return $result;
+        }
 
         // メインルーチン
+        $result = add(40, 40);
+        echo $result;
 
         // バブルソート
-        $array=array(9,2,8,3,7,4,6,5,1);
-        $length=count($array);
+        $array = array(9,2,8,3,7,4,6,5,1);
+        $length = count($array);
+
+        // 配列の長さだけループ
+        for ($i=0 ; $i<$length ; $i++){
+
+        }
 
 
     ?>
