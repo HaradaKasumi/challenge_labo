b77cb31f34b4fb671ec825f2cfc6336232e59fe6
HaradaKasumi <harada@key-p.jp>
2021-02-28 18:27:07 +0900

    20210228

--- chapter02/function/nest.php
+++ chapter02/function/nest.php
@@ -29,16 +29,51 @@
         // メインルーチン
         $result = add(40, 40);
         echo $result;
+        echo "<br />";
 
-        // バブルソート
-        $array = array(9,2,8,3,7,4,6,5,1);
-        $length = count($array);
 
-        // 配列の長さだけループ
-        for ($i=0 ; $i<$length ; $i++){
 
+    // at coderの問題　ABC086A - Product  /積が奇数なら Odd と、 偶数なら Even と出力せよ。
+        function OddOrEven(int $a, int $b):string
+        {
+            if (!is_numeric($a) || !is_numeric($b)){
+                return '値が不正';
+            }
+
+            $score = $a + $b;
+            if ($score % 2 === 0 ){
+                return 'Even';
+            } else{
+                return 'Odd';
+            }
         }
 
+
+        $result = OddOrEven(5,6);
+        echo $result;
+        echo "<br />";
+
+        // at coderの問題　ABC081A - Placing Marbles  1が出力された回数
+        function checkOutput(int $input):int
+        {
+            $array = str_split($input);
+
+            $length = count($array);
+            $total = 0;
+
+            for($i = 0; $i <= $length-1; $i++){
+                if ($array[$i] == 1){ // ===だったとき
+                    $total = $total +1;
+                }elseif($array[$i] == 0){
+                    continue;
+                }
+            }
+            return $total;
+        }
+
+        $test = checkOutput(10101);
+        echo $test;
+        echo "<br />";
 
     ?>
 </body>
